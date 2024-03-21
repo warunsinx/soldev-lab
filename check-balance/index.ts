9568e943e5e1470ba5bfbed5d7f0d13499ae84ee
warunsinx <warunsinx@gmail.com>
2024-03-22 00:35:38 +0700

    feat: :sparkles: learned sol, lamports, how to fetch balance, sns

---
+++ check-balance/index.ts
@@ -0,0 +1,34 @@
+import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
+import { resolve } from "@bonfida/spl-name-service";
+
+const connection = new Connection(clusterApiUrl("devnet"));
+console.log(`✅ Connected to devnet !`);
+
+const parseAccount = (address: string) => {
+  try {
+    return new PublicKey(address);
+  } catch (e) {
+    throw new Error(`Invalid public key: ${address}`);
+  }
+};
+
+const checkBalance = async (account: PublicKey) => {
+  const address = parseAccount(account.toBase58());
+  console.log(`✅ Checking balance of ${address.toBase58()}...`);
+  const balance = await connection.getBalance(address);
+  console.log(`✅ Balance in Lamports: ${balance}`);
+  const lamportsPerSol = 1000000000;
+  const balanceInSol = balance / lamportsPerSol;
+  console.log(`✅ Balance in SOL: ${balanceInSol}`);
+};
+
+const account1 = new PublicKey("tDN8dHGLWsc5ZK1ybsQfSNaFXfHsE5MzwmUVARykxbv");
+checkBalance(account1);
+
+const connectionMainnet = new Connection(clusterApiUrl("mainnet-beta"));
+console.log(`✅ Connected to Mainnet-Beta !`);
+
+const name = "mccann";
+resolve(connectionMainnet, name).then((address) => {
+  checkBalance(address);
+});

0ad39b99f4a684919de4bee3a61ec3f791c187a8
warunsinx <warunsinx@gmail.com>
2024-03-24 19:24:54 +0700

    feat: :sparkles: update on send-tx

---
+++ send-tx/transfer.ts
@@ -0,0 +1,40 @@
+import {
+  Connection,
+  Transaction,
+  SystemProgram,
+  sendAndConfirmTransaction,
+  PublicKey,
+} from "@solana/web3.js";
+
+import "dotenv/config";
+
+import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+
+const suppliedToPubkey = process.argv[2] || null;
+
+if (!suppliedToPubkey) {
+  console.error("Error: No destination pubkey supplied");
+  process.exit(1);
+}
+
+const senderKeypair = getKeypairFromEnvironment("SECRET_KEY");
+
+const toPubkey = new PublicKey(suppliedToPubkey);
+
+const connection = new Connection("https://api.devnet.solana.com", "confirmed");
+
+const LAMPORTS_TO_SEND = 5000;
+
+const transaction = new Transaction();
+
+const sendSolInstruction = SystemProgram.transfer({
+  fromPubkey: senderKeypair.publicKey,
+  toPubkey,
+  lamports: LAMPORTS_TO_SEND,
+});
+
+transaction.add(sendSolInstruction);
+
+const signature = await sendAndConfirmTransaction(connection, transaction, [
+  senderKeypair,
+]);
